599b4499c9cd011503cfdca689e6b0effc704758
dinuka <dinukasal@gmail.com>
2018-01-17 16:44:15 +0000

    added api request

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,36 @@
-var appRouter = function(app) {
+var https = require('https');
+https.post = require('https-post');
 
-app.get("/", function(req, res) {
-    res.send("Hello World");
-}); 
+var appRouter = function (app) {
 
-app.post("/account", function(req, res) {
-    if(!req.body.username || !req.body.password || !req.body.twitter) {
-        return res.send({"status": "error", "message": "missing a parameter"});
-    } else {
-        return res.send(req.body);
-    }
-});
+    app.get("/", function (req, res) {
+        res.send("Hello World");
+    });
+
+    app.post("/account", function (req, res) {
+        if (!req.body.username || !req.body.password || !req.body.twitter) {
+            return res.send({ "status": "error", "message": "missing a parameter" });
+        } else {
+            return res.send(req.body);
+        }
+    });
+
+
+
+    app.post("/", function (req, res) {
+        //res.send(req.body);
+        https.post('https://us-central1-smart-comm.cloudfunctions.net/updateLocation',
+            { "car": "001", "lat": req.body.lat, "long": req.body.long },
+            function (res) {
+                res.setEncoding('utf8');
+                res.on('data', function (chunk) {
+                    console.log(chunk);
+
+                });
+            });
+        res.send(req.body)
+    })
 
 }
- 
+
 module.exports = appRouter;
-
-
